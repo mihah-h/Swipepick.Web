1be02ba966abd602f6a39ae6d61b8592d29739bc
Михаил <gmike103g@gmail.com>
2023-05-29 08:49:55 +0500

    bild fix

--- src/app/shared/components/question-tooltip/question-tooltip.component.ts
+++ src/app/shared/components/question-tooltip/question-tooltip.component.ts
@@ -27,7 +27,27 @@ export class QuestionTooltipComponent {
   }
 
   get correctAnswersPercent() {
-    if (!this.questionStatistic.correctAnswersPercent || this.questionStatistic.correctAnswersPercent === 100) {
+    if (!this.questionStatistic.correctAnswersPercent
+      || this.questionStatistic.correctAnswersPercent === 0
+      || this.questionStatistic.correctAnswersPercent === 5
+      || this.questionStatistic.correctAnswersPercent === 10
+      || this.questionStatistic.correctAnswersPercent === 15
+      || this.questionStatistic.correctAnswersPercent === 25
+      || this.questionStatistic.correctAnswersPercent === 30
+      || this.questionStatistic.correctAnswersPercent === 35
+      || this.questionStatistic.correctAnswersPercent === 40
+      || this.questionStatistic.correctAnswersPercent === 45
+      || this.questionStatistic.correctAnswersPercent === 50
+      || this.questionStatistic.correctAnswersPercent === 55
+      || this.questionStatistic.correctAnswersPercent === 60
+      || this.questionStatistic.correctAnswersPercent === 65
+      || this.questionStatistic.correctAnswersPercent === 70
+      || this.questionStatistic.correctAnswersPercent === 75
+      || this.questionStatistic.correctAnswersPercent === 80
+      || this.questionStatistic.correctAnswersPercent === 85
+      || this.questionStatistic.correctAnswersPercent === 90
+      || this.questionStatistic.correctAnswersPercent === 95
+      || this.questionStatistic.correctAnswersPercent === 100) {
       return this.questionStatistic.correctAnswersPercent | 0
     }
     return (this.questionStatistic.correctAnswersPercent | 0) + 1
